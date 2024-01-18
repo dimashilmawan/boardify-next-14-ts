2f186c0b1430ee817516b6abfd6f503f8a631c7c
Dimas hilmawan <hilmawan22@gmail.com>
2024-01-18 08:56:40 +0700

    finish card actions component

--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -1,16 +1,91 @@
 "use client";
 
+import { copyCard } from "@/actions/copy-card";
+import { deleteCard } from "@/actions/delete-card";
+import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useAction } from "@/hooks/use-action";
+import { useCardModal } from "@/hooks/use-card-modal";
 import { CardWithList } from "@/types";
+import { Copy, Trash } from "lucide-react";
+import { useParams } from "next/navigation";
+import { toast } from "sonner";
 
 export const Actions = ({ data }: { data: CardWithList }) => {
-  return <div>Actions</div>;
+  const onClose = useCardModal((state) => state.onClose);
+  const params = useParams();
+
+  const { execute: executeCopyCard, isLoading: isLoadingCopy } = useAction(
+    copyCard,
+    {
+      onSuccess(data) {
+        toast.success(`Card '${data.title}' copied`);
+        onClose();
+      },
+      onError(error) {
+        toast.error(error);
+      },
+    },
+  );
+  const { execute: executeDeleteCard, isLoading: isLoadingDelete } = useAction(
+    deleteCard,
+    {
+      onSuccess(data) {
+        toast.success(`Card '${data.title}' deleted`);
+        onClose();
+      },
+      onError(error) {
+        toast.error(error);
+      },
+    },
+  );
+
+  function onCopy() {
+    const boardId = params.id as string;
+
+    executeCopyCard({ boardId, id: data.id });
+  }
+  function onDelete() {
+    const boardId = params.id as string;
+
+    executeDeleteCard({ boardId, id: data.id });
+  }
+
+  return (
+    <div className="h-full ">
+      <p className="text-xs font-semibold">Actions</p>
+      <div className="mt-4 space-y-2">
+        <Button
+          onClick={onCopy}
+          size="inline"
+          className="w-full justify-start gap-2"
+          variant="gray"
+          disabled={isLoadingCopy}
+        >
+          <Copy className="h-5 w-5" />
+          <span>Copy</span>
+        </Button>
+        <Button
+          onClick={onDelete}
+          size="inline"
+          className="w-full justify-start gap-2"
+          variant="destructive"
+          disabled={isLoadingDelete}
+        >
+          <Trash className="h-5 w-5" />
+          <span>Delete</span>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 Actions.Skeleton = function ActionsSkeleton() {
   return (
-    <div>
-      <Skeleton />
-    </div>
+    <>
+      <Skeleton className=" h-6 w-full" />
+      <Skeleton className="mt-4 h-8 w-full" />
+      <Skeleton className="mt-2 h-8 w-full" />
+    </>
   );
 };
